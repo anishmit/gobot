f437dfa7e826ccbf7dc3ba3783a664ff26d213b7
am <novmitraa40@gmail.com>
2025-02-10 05:24:08 -0500

    Tried to fix bad quality of audio but it doesn't work

--- interactions/youtube.go
+++ interactions/youtube.go
@@ -28,7 +28,7 @@ func init() {
 			return
 		}
 		cmd1 := exec.Command("./yt-dlp", "-o", "-", "https://www.youtube.com/watch?v=k8JflBNovLE&pp=ygUUcG93ZXIga2FueWUgZXhwbGNpaXQ%3D", "-f", "bestaudio")
-		cmd2 := exec.Command("ffmpeg", "-i", "-", "-c:a", "libopus", "-b:a", "96K", "-ar", "48000", "-ac", "2", "-f", "ogg", "-")
+		cmd2 := exec.Command("ffmpeg", "-i", "-", "-c:a", "libopus", "-b:a", "96K", "-ar", "48000", "-ac", "2", "-f", "opus", "-")
 		cmd2.Stdin, err = cmd1.StdoutPipe()
 		if err != nil {
 			log.Println("Could not get command 1 standard output pipe", err)
@@ -47,16 +47,39 @@ func init() {
 		}
 		decoder := ogg.NewDecoder(pipe)
 		voice.Speaking(true)
+		var lastPacket []byte
 		for {
 			page, err := decoder.Decode()
 			if err != nil {
 				log.Println("Could not decode", err)
 				break
 			}
-			for _, packet := range page.Packets {
-				voice.OpusSend <- packet
+			continuation := page.Type & ogg.COP != 0
+			
+			if len(page.Packets) == 1 && continuation {
+				lastPacket = append(lastPacket, page.Packets[0]...)
+				continue
+			} else if continuation {
+				voice.OpusSend <- append(lastPacket, page.Packets[0]...)
+			} else {
+				if lastPacket != nil {
+					voice.OpusSend <- lastPacket
+				}
+				if len(page.Packets) == 1 {
+					lastPacket = page.Packets[0]
+					continue
+				} else {
+					voice.OpusSend <- page.Packets[0]
+				}
 			}
+			
+			for i := 1; i < len(page.Packets) - 1; i++ {
+				voice.OpusSend <- page.Packets[i]
+			}
+
+			lastPacket = page.Packets[len(page.Packets) - 1]
 		}
+		voice.OpusSend <- lastPacket
 		voice.Speaking(false)
 		cmd2.Wait()
 		cmd1.Wait()
